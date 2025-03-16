12dc4f404fceb21ff9364d026aeeb42e5ed95a7d
KaiSong06 <kai.song421@gmail.com>
2025-03-15 21:13:01 -0400

    Search

---
+++ Schollux/src/app/search.py
@@ -0,0 +1,14 @@
+from scholarly import scholarly
+
+def search(category, query):
+    res = []
+    search_results = scholarly.search_pubs(query)
+    counter = 0
+    if category == "topic":
+        while counter < 5:
+            title = next(search_results).get("bib", {}).get("title", "No title available")
+            authors = next(search_results).get("bib", {}).get("author", "No authors listed")
+            if title != "No title available" and authors != "No authors listed":
+                res.append([title, authors])
+                counter += 1
+    return res
